23a59c929de52836e1899386192786c663bb5e17
jannfis <jann@mistrust.net>
2020-08-06 21:08:06 +0200

    fix: Typo in annotation names for Helm target parameters

--- pkg/common/constants.go
+++ pkg/common/constants.go
@@ -6,9 +6,9 @@ package common
 // allowed for updates.
 const ImageUpdaterAnnotation = "argocd-image-updater.argoproj.io/image-list"
 
-const HelmParamImageNameAnnotation = "argocd-image-update.argoproj.io/%s.image-name"
-const HelmParamImageTagAnnotation = "argocd-image-update.argoproj.io/%s.image-tag"
-const HelmParamImageSpecAnnotation = "argocd-image-update.argoproj.io/%s.image-spec"
+const HelmParamImageNameAnnotation = "argocd-image-updater.argoproj.io/%s.image-name"
+const HelmParamImageTagAnnotation = "argocd-image-updater.argoproj.io/%s.image-tag"
+const HelmParamImageSpecAnnotation = "argocd-image-updater.argoproj.io/%s.image-spec"
 
 // gcr.io=secret:argocd/mysecret,docker.io=env:FOOBAR
 const SecretListAnnotation = "argocd-image-updater.argoproj.io/pullsecrets"
