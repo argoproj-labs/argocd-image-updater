fc6183d4d819d7341616100047fcee42b7c85dba
KS. Yim <ks.yim@linecorp.com>
2023-06-09 18:21:14 +0900

    Add helm override commit test
    Signed-off-by: KS. Yim <ks.yim@linecorp.com>

--- pkg/argocd/update_test.go
+++ pkg/argocd/update_test.go
@@ -1823,6 +1823,56 @@ func Test_CommitUpdates(t *testing.T) {
 		assert.NoError(t, err)
 	})
 
+	t.Run("Good commit to helm override", func(t *testing.T) {
+		app := app.DeepCopy()
+		app.Status.SourceType = "Helm"
+		app.Spec.Source.Helm = &v1alpha1.ApplicationSourceHelm{Parameters: []v1alpha1.HelmParameter{
+			{Name: "bar", Value: "bar", ForceString: true},
+			{Name: "baz", Value: "baz", ForceString: true},
+		}}
+		gitMock, dir, cleanup := mockGit(t)
+		defer cleanup()
+		of := filepath.Join(dir, ".argocd-source-testapp.yaml")
+		assert.NoError(t, os.WriteFile(of, []byte(`
+helm:
+  parameters:
+  - name: foo
+    value: foo
+    forcestring: true
+`), os.ModePerm))
+
+		gitMock.On("Checkout", mock.Anything).Run(func(args mock.Arguments) {
+			args.Assert(t, "mydefaultbranch")
+		}).Return(nil)
+		gitMock.On("Add", mock.Anything).Return(nil)
+		gitMock.On("Commit", mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil)
+		gitMock.On("Push", mock.Anything, mock.Anything, mock.Anything).Return(nil)
+		gitMock.On("SymRefToBranch", mock.Anything).Return("mydefaultbranch", nil)
+		wbc, err := getWriteBackConfig(app, &kubeClient, &argoClient)
+		require.NoError(t, err)
+		wbc.GitClient = gitMock
+		app.Spec.Source.TargetRevision = "HEAD"
+		wbc.GitBranch = ""
+
+		err = commitChanges(app, wbc, nil)
+		assert.NoError(t, err)
+		override, err := os.ReadFile(of)
+		assert.NoError(t, err)
+		assert.YAMLEq(t, `
+helm:
+  parameters:
+  - name: foo
+    value: foo
+    forcestring: true
+  - name: bar
+    value: bar
+    forcestring: true
+  - name: baz
+    value: baz
+    forcestring: true
+`, string(override))
+	})
+
 	t.Run("Good commit to kustomization", func(t *testing.T) {
 		app := app.DeepCopy()
 		app.Annotations[common.WriteBackTargetAnnotation] = "kustomization"
