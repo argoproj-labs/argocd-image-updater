1baee46e6fd3283f1909bf57210e773282b8b90c
Mangaal <angommeeteimangaal@gmail.com>
2024-07-01 20:41:25 +0530

    unit test for getKubeConfig
    Signed-off-by: Mangaal <angommeeteimangaal@gmail.com>

--- cmd/util_test.go
+++ cmd/util_test.go
@@ -1,9 +1,37 @@
 package main
 
 import (
+	"context"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestGetKubeConfig(t *testing.T) {
+	t.Run("valid kubeConfig", func(t *testing.T) {
+		client, err := getKubeConfig(context.TODO(), "", "../test/testdata/kubernetes/config")
+		require.NoError(t, err)
+		assert.NotNil(t, client)
+		assert.Equal(t, "default", client.Namespace)
+	})
+
+	t.Run("invalid kubeConfig", func(t *testing.T) {
+		_, err := getKubeConfig(context.TODO(), "", "invalid/kubernetes/config")
+		require.Error(t, err)
+
+	})
+
+	t.Run("valid kubeConfig with ns", func(t *testing.T) {
+		client, err := getKubeConfig(context.TODO(), "argocd", "../test/testdata/kubernetes/config")
+		require.NoError(t, err)
+		assert.NotNil(t, client)
+		assert.Equal(t, "argocd", client.Namespace)
+
+	})
+
+}
 
 func TestGetPrintableInterval(t *testing.T) {
 	tests := []struct {
