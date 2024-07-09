53ad1983985dc8ae65c86135aac6b2867b7d1855
Cheng Fang <cfang@redhat.com>
2024-07-08 21:57:10 -0400

    tests: add unit tests for pkg/cache
    Signed-off-by: Cheng Fang <cfang@redhat.com>

--- pkg/cache/memcache_test.go
+++ pkg/cache/memcache_test.go
@@ -4,10 +4,11 @@ import (
 	"testing"
 	"time"
 
-	"github.com/argoproj-labs/argocd-image-updater/pkg/tag"
-
+	memcache "github.com/patrickmn/go-cache"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/argoproj-labs/argocd-image-updater/pkg/tag"
 )
 
 func Test_MemCache(t *testing.T) {
@@ -22,12 +23,14 @@ func Test_MemCache(t *testing.T) {
 		require.NotNil(t, cachedTag)
 		assert.Equal(t, imageTag, cachedTag.TagName)
 		assert.True(t, mc.HasTag(imageName, imageTag))
+		assert.Equal(t, 1, mc.NumEntries())
 	})
 
 	t.Run("Cache miss", func(t *testing.T) {
 		mc := NewMemCache()
 		newTag := tag.NewImageTag(imageTag, time.Unix(0, 0), "")
 		mc.SetTag(imageName, newTag)
+		assert.Equal(t, 1, mc.NumEntries())
 		cachedTag, err := mc.GetTag(imageName, "v1.0.1")
 		require.NoError(t, err)
 		require.Nil(t, cachedTag)
@@ -43,9 +46,25 @@ func Test_MemCache(t *testing.T) {
 		require.NotNil(t, cachedTag)
 		assert.Equal(t, imageTag, cachedTag.TagName)
 		assert.True(t, mc.HasTag(imageName, imageTag))
+		assert.Equal(t, 1, mc.NumEntries())
 		mc.ClearCache()
+		assert.Equal(t, 0, mc.NumEntries())
 		cachedTag, err = mc.GetTag(imageName, imageTag)
 		require.NoError(t, err)
 		require.Nil(t, cachedTag)
+	})
+	t.Run("Image Cache Key", func(t *testing.T) {
+		mc := MemCache{
+			cache: memcache.New(0, 0),
+		}
+		application := "application1"
+		key := imageCacheKey(imageName)
+		mc.SetImage(imageName, application)
+		app, b := mc.cache.Get(key)
+		assert.True(t, b)
+		assert.Equal(t, application, app)
+		assert.Equal(t, 1, mc.NumEntries())
+		mc.ClearCache()
+		assert.Equal(t, 0, mc.NumEntries())
 	})
 }
