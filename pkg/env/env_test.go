3e7378a8e62b56d94a5d786536894a87eda3137d
Ishita Sequeira <46771830+ishitasequeira@users.noreply.github.com>
2024-10-14 11:07:11 -0400

    chore: Address comments in PR #854 (#886)
    Signed-off-by: Ishita Sequeira <ishiseq29@gmail.com>

--- pkg/env/env_test.go
+++ pkg/env/env_test.go
@@ -36,3 +36,30 @@ func Test_GetStringVal(t *testing.T) {
 		assert.Equal(t, "invalid", GetStringVal("TEST_STRING_VAL", "invalid"))
 	})
 }
+
+func Test_ParseNumFromEnv(t *testing.T) {
+	t.Run("Get number from existing env var within range", func(t *testing.T) {
+		_ = os.Setenv("TEST_NUM_VAL", "5")
+		defer os.Setenv("TEST_NUM_VAL", "")
+		assert.Equal(t, 5, ParseNumFromEnv("TEST_NUM_VAL", 0, 1, 10))
+	})
+	t.Run("Get default value from non-existing env var", func(t *testing.T) {
+		_ = os.Setenv("TEST_NUM_VAL", "")
+		assert.Equal(t, 10, ParseNumFromEnv("TEST_NUM_VAL", 10, 1, 20))
+	})
+	t.Run("Get default value from env var with non-numeric value", func(t *testing.T) {
+		_ = os.Setenv("TEST_NUM_VAL", "abc")
+		defer os.Setenv("TEST_NUM_VAL", "")
+		assert.Equal(t, 10, ParseNumFromEnv("TEST_NUM_VAL", 10, 1, 20))
+	})
+	t.Run("Get default value from env var with value less than min", func(t *testing.T) {
+		_ = os.Setenv("TEST_NUM_VAL", "0")
+		defer os.Setenv("TEST_NUM_VAL", "")
+		assert.Equal(t, 10, ParseNumFromEnv("TEST_NUM_VAL", 10, 1, 20))
+	})
+	t.Run("Get default value from env var with value greater than max", func(t *testing.T) {
+		_ = os.Setenv("TEST_NUM_VAL", "30")
+		defer os.Setenv("TEST_NUM_VAL", "")
+		assert.Equal(t, 10, ParseNumFromEnv("TEST_NUM_VAL", 10, 1, 20))
+	})
+}
