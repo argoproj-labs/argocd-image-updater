403081d49167827508f8a2df5b328775aa6ad178
Ishita Sequeira <ishiseq29@gmail.com>
2024-11-12 07:17:25 -0500

    Add cache package to registry-scanner
    Signed-off-by: Ishita Sequeira <ishiseq29@gmail.com>

---
+++ registry-scanner/pkg/cache/memcache_test.go
@@ -0,0 +1,70 @@
+package cache
+
+import (
+	"testing"
+	"time"
+
+	memcache "github.com/patrickmn/go-cache"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/argoproj-labs/argocd-image-updater/pkg/tag"
+)
+
+func Test_MemCache(t *testing.T) {
+	imageName := "foo/bar"
+	imageTag := "v1.0.0"
+	t.Run("Cache hit", func(t *testing.T) {
+		mc := NewMemCache()
+		newTag := tag.NewImageTag(imageTag, time.Unix(0, 0), "")
+		mc.SetTag(imageName, newTag)
+		cachedTag, err := mc.GetTag(imageName, imageTag)
+		require.NoError(t, err)
+		require.NotNil(t, cachedTag)
+		assert.Equal(t, imageTag, cachedTag.TagName)
+		assert.True(t, mc.HasTag(imageName, imageTag))
+		assert.Equal(t, 1, mc.NumEntries())
+	})
+
+	t.Run("Cache miss", func(t *testing.T) {
+		mc := NewMemCache()
+		newTag := tag.NewImageTag(imageTag, time.Unix(0, 0), "")
+		mc.SetTag(imageName, newTag)
+		assert.Equal(t, 1, mc.NumEntries())
+		cachedTag, err := mc.GetTag(imageName, "v1.0.1")
+		require.NoError(t, err)
+		require.Nil(t, cachedTag)
+		assert.False(t, mc.HasTag(imageName, "v1.0.1"))
+	})
+
+	t.Run("Cache clear", func(t *testing.T) {
+		mc := NewMemCache()
+		newTag := tag.NewImageTag(imageTag, time.Unix(0, 0), "")
+		mc.SetTag(imageName, newTag)
+		cachedTag, err := mc.GetTag(imageName, imageTag)
+		require.NoError(t, err)
+		require.NotNil(t, cachedTag)
+		assert.Equal(t, imageTag, cachedTag.TagName)
+		assert.True(t, mc.HasTag(imageName, imageTag))
+		assert.Equal(t, 1, mc.NumEntries())
+		mc.ClearCache()
+		assert.Equal(t, 0, mc.NumEntries())
+		cachedTag, err = mc.GetTag(imageName, imageTag)
+		require.NoError(t, err)
+		require.Nil(t, cachedTag)
+	})
+	t.Run("Image Cache Key", func(t *testing.T) {
+		mc := MemCache{
+			cache: memcache.New(0, 0),
+		}
+		application := "application1"
+		key := imageCacheKey(imageName)
+		mc.SetImage(imageName, application)
+		app, b := mc.cache.Get(key)
+		assert.True(t, b)
+		assert.Equal(t, application, app)
+		assert.Equal(t, 1, mc.NumEntries())
+		mc.ClearCache()
+		assert.Equal(t, 0, mc.NumEntries())
+	})
+}
---
+++ registry-scanner/pkg/kube/kubernetes.go
@@ -0,0 +1,85 @@
+package kube
+
+// Kubernetes client related code
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+type KubernetesClient struct {
+	Clientset kubernetes.Interface
+	Context   context.Context
+	Namespace string
+}
+
+func NewKubernetesClient(ctx context.Context, client kubernetes.Interface, namespace string) *KubernetesClient {
+	kc := &KubernetesClient{}
+	kc.Context = ctx
+	kc.Clientset = client
+	kc.Namespace = namespace
+	return kc
+}
+
+// NewKubernetesClient creates a new Kubernetes client object from given
+// configuration file. If configuration file is the empty string, in-cluster
+// client will be created.
+func NewKubernetesClientFromConfig(ctx context.Context, namespace string, kubeconfig string) (*KubernetesClient, error) {
+	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
+	loadingRules.DefaultClientConfig = &clientcmd.DefaultClientConfig
+	loadingRules.ExplicitPath = kubeconfig
+	overrides := clientcmd.ConfigOverrides{}
+	clientConfig := clientcmd.NewInteractiveDeferredLoadingClientConfig(loadingRules, &overrides, os.Stdin)
+
+	config, err := clientConfig.ClientConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	if namespace == "" {
+		namespace, _, err = clientConfig.Namespace()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	applicationsClientset, err := versioned.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewKubernetesClient(ctx, clientset, applicationsClientset, namespace), nil
+}
+
+// GetSecretData returns the raw data from named K8s secret in given namespace
+func (client *KubernetesClient) GetSecretData(namespace string, secretName string) (map[string][]byte, error) {
+	secret, err := client.Clientset.CoreV1().Secrets(namespace).Get(client.Context, secretName, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return secret.Data, nil
+}
+
+// GetSecretField returns the value of a field from named K8s secret in given namespace
+func (client *KubernetesClient) GetSecretField(namespace string, secretName string, field string) (string, error) {
+	secret, err := client.GetSecretData(namespace, secretName)
+	if err != nil {
+		return "", err
+	}
+	if data, ok := secret[field]; !ok {
+		return "", fmt.Errorf("secret '%s/%s' does not have a field '%s'", namespace, secretName, field)
+	} else {
+		return string(data), nil
+	}
+}
