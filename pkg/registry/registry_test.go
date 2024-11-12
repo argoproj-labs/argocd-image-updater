d01c754c2a7baa9d275b06055753572c45904767
Ishita Sequeira <ishiseq29@gmail.com>
2024-11-12 07:19:53 -0500

    revert changes to test file
    Signed-off-by: Ishita Sequeira <ishiseq29@gmail.com>

--- pkg/registry/registry_test.go
+++ pkg/registry/registry_test.go
@@ -5,108 +5,112 @@ import (
 	"testing"
 	"time"
 
-	// "github.com/argoproj-labs/argocd-image-updater/pkg/registry/mocks"
+	"github.com/argoproj-labs/argocd-image-updater/pkg/image"
+	"github.com/argoproj-labs/argocd-image-updater/pkg/options"
+	"github.com/argoproj-labs/argocd-image-updater/pkg/registry/mocks"
+	"github.com/argoproj-labs/argocd-image-updater/pkg/tag"
 
-	//nolint:staticcheck
+	"github.com/distribution/distribution/v3/manifest/schema1" //nolint:staticcheck
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
 func Test_GetTags(t *testing.T) {
 
-	// t.Run("Check for correctly returned tags with semver sort", func(t *testing.T) {
-	// 	regClient := mocks.RegistryClient{}
-	// 	regClient.On("NewRepository", mock.Anything).Return(nil)
-	// 	regClient.On("Tags", mock.Anything).Return([]string{"1.2.0", "1.2.1", "1.2.2"}, nil)
+	t.Run("Check for correctly returned tags with semver sort", func(t *testing.T) {
+		regClient := mocks.RegistryClient{}
+		regClient.On("NewRepository", mock.Anything).Return(nil)
+		regClient.On("Tags", mock.Anything).Return([]string{"1.2.0", "1.2.1", "1.2.2"}, nil)
 
-	// 	ep, err := GetRegistryEndpoint("")
-	// 	require.NoError(t, err)
+		ep, err := GetRegistryEndpoint("")
+		require.NoError(t, err)
 
-	// 	img := image.NewFromIdentifier("foo/bar:1.2.0")
+		img := image.NewFromIdentifier("foo/bar:1.2.0")
 
-	// 	tl, err := ep.GetTags(img, &regClient, &image.VersionConstraint{Strategy: image.StrategySemVer, Options: options.NewManifestOptions()})
-	// 	require.NoError(t, err)
-	// 	assert.NotEmpty(t, tl)
+		tl, err := ep.GetTags(img, &regClient, &image.VersionConstraint{Strategy: image.StrategySemVer, Options: options.NewManifestOptions()})
+		require.NoError(t, err)
+		assert.NotEmpty(t, tl)
 
-	// 	tag, err := ep.Cache.GetTag("foo/bar", "1.2.1")
-	// 	require.NoError(t, err)
-	// 	assert.Nil(t, tag)
-	// })
+		tag, err := ep.Cache.GetTag("foo/bar", "1.2.1")
+		require.NoError(t, err)
+		assert.Nil(t, tag)
+	})
 
-	// t.Run("Check for correctly returned tags with filter function applied", func(t *testing.T) {
-	// 	regClient := mocks.RegistryClient{}
-	// 	regClient.On("NewRepository", mock.Anything).Return(nil)
-	// 	regClient.On("Tags", mock.Anything).Return([]string{"1.2.0", "1.2.1", "1.2.2"}, nil)
+	t.Run("Check for correctly returned tags with filter function applied", func(t *testing.T) {
+		regClient := mocks.RegistryClient{}
+		regClient.On("NewRepository", mock.Anything).Return(nil)
+		regClient.On("Tags", mock.Anything).Return([]string{"1.2.0", "1.2.1", "1.2.2"}, nil)
 
-	// 	ep, err := GetRegistryEndpoint("")
-	// 	require.NoError(t, err)
+		ep, err := GetRegistryEndpoint("")
+		require.NoError(t, err)
 
-	// 	img := image.NewFromIdentifier("foo/bar:1.2.0")
+		img := image.NewFromIdentifier("foo/bar:1.2.0")
 
-	// 	tl, err := ep.GetTags(img, &regClient, &image.VersionConstraint{
-	// 		Strategy:  image.StrategySemVer,
-	// 		MatchFunc: image.MatchFuncNone,
-	// 		Options:   options.NewManifestOptions()})
-	// 	require.NoError(t, err)
-	// 	assert.Empty(t, tl.Tags())
+		tl, err := ep.GetTags(img, &regClient, &image.VersionConstraint{
+			Strategy:  image.StrategySemVer,
+			MatchFunc: image.MatchFuncNone,
+			Options:   options.NewManifestOptions()})
+		require.NoError(t, err)
+		assert.Empty(t, tl.Tags())
 
-	// 	tag, err := ep.Cache.GetTag("foo/bar", "1.2.1")
-	// 	require.NoError(t, err)
-	// 	assert.Nil(t, tag)
-	// })
+		tag, err := ep.Cache.GetTag("foo/bar", "1.2.1")
+		require.NoError(t, err)
+		assert.Nil(t, tag)
+	})
 
-	// t.Run("Check for correctly returned tags with name sort", func(t *testing.T) {
+	t.Run("Check for correctly returned tags with name sort", func(t *testing.T) {
 
-	// 	regClient := mocks.RegistryClient{}
-	// 	regClient.On("NewRepository", mock.Anything).Return(nil)
-	// 	regClient.On("Tags", mock.Anything).Return([]string{"1.2.0", "1.2.1", "1.2.2"}, nil)
+		regClient := mocks.RegistryClient{}
+		regClient.On("NewRepository", mock.Anything).Return(nil)
+		regClient.On("Tags", mock.Anything).Return([]string{"1.2.0", "1.2.1", "1.2.2"}, nil)
 
-	// 	ep, err := GetRegistryEndpoint("")
-	// 	require.NoError(t, err)
+		ep, err := GetRegistryEndpoint("")
+		require.NoError(t, err)
 
-	// 	img := image.NewFromIdentifier("foo/bar:1.2.0")
+		img := image.NewFromIdentifier("foo/bar:1.2.0")
 
-	// 	tl, err := ep.GetTags(img, &regClient, &image.VersionConstraint{Strategy: image.StrategyAlphabetical, Options: options.NewManifestOptions()})
-	// 	require.NoError(t, err)
-	// 	assert.NotEmpty(t, tl)
+		tl, err := ep.GetTags(img, &regClient, &image.VersionConstraint{Strategy: image.StrategyAlphabetical, Options: options.NewManifestOptions()})
+		require.NoError(t, err)
+		assert.NotEmpty(t, tl)
 
-	// 	tag, err := ep.Cache.GetTag("foo/bar", "1.2.1")
-	// 	require.NoError(t, err)
-	// 	assert.Nil(t, tag)
-	// })
+		tag, err := ep.Cache.GetTag("foo/bar", "1.2.1")
+		require.NoError(t, err)
+		assert.Nil(t, tag)
+	})
 
-	// t.Run("Check for correctly returned tags with latest sort", func(t *testing.T) {
-	// 	ts := "2006-01-02T15:04:05.999999999Z"
-	// 	meta1 := &schema1.SignedManifest{ //nolint:staticcheck
-	// 		Manifest: schema1.Manifest{ //nolint:staticcheck
-	// 			History: []schema1.History{ //nolint:staticcheck
-	// 				{
-	// 					V1Compatibility: `{"created":"` + ts + `"}`,
-	// 				},
-	// 			},
-	// 		},
-	// 	}
+	t.Run("Check for correctly returned tags with latest sort", func(t *testing.T) {
+		ts := "2006-01-02T15:04:05.999999999Z"
+		meta1 := &schema1.SignedManifest{ //nolint:staticcheck
+			Manifest: schema1.Manifest{ //nolint:staticcheck
+				History: []schema1.History{ //nolint:staticcheck
+					{
+						V1Compatibility: `{"created":"` + ts + `"}`,
+					},
+				},
+			},
+		}
 
-	// 	regClient := mocks.RegistryClient{}
-	// 	regClient.On("NewRepository", mock.Anything).Return(nil)
-	// 	regClient.On("Tags", mock.Anything).Return([]string{"1.2.0", "1.2.1", "1.2.2"}, nil)
-	// 	regClient.On("ManifestForTag", mock.Anything, mock.Anything).Return(meta1, nil)
-	// 	regClient.On("TagMetadata", mock.Anything, mock.Anything).Return(&tag.TagInfo{}, nil)
+		regClient := mocks.RegistryClient{}
+		regClient.On("NewRepository", mock.Anything).Return(nil)
+		regClient.On("Tags", mock.Anything).Return([]string{"1.2.0", "1.2.1", "1.2.2"}, nil)
+		regClient.On("ManifestForTag", mock.Anything, mock.Anything).Return(meta1, nil)
+		regClient.On("TagMetadata", mock.Anything, mock.Anything).Return(&tag.TagInfo{}, nil)
 
-	// 	ep, err := GetRegistryEndpoint("")
-	// 	require.NoError(t, err)
-	// 	ep.Cache.ClearCache()
+		ep, err := GetRegistryEndpoint("")
+		require.NoError(t, err)
+		ep.Cache.ClearCache()
 
-	// 	img := image.NewFromIdentifier("foo/bar:1.2.0")
-	// 	tl, err := ep.GetTags(img, &regClient, &image.VersionConstraint{Strategy: image.StrategyNewestBuild, Options: options.NewManifestOptions()})
-	// 	require.NoError(t, err)
-	// 	assert.NotEmpty(t, tl)
+		img := image.NewFromIdentifier("foo/bar:1.2.0")
+		tl, err := ep.GetTags(img, &regClient, &image.VersionConstraint{Strategy: image.StrategyNewestBuild, Options: options.NewManifestOptions()})
+		require.NoError(t, err)
+		assert.NotEmpty(t, tl)
 
-	// 	tag, err := ep.Cache.GetTag("foo/bar", "1.2.1")
-	// 	require.NoError(t, err)
-	// 	require.NotNil(t, tag)
-	// 	require.Equal(t, "1.2.1", tag.TagName)
-	// })
+		tag, err := ep.Cache.GetTag("foo/bar", "1.2.1")
+		require.NoError(t, err)
+		require.NotNil(t, tag)
+		require.Equal(t, "1.2.1", tag.TagName)
+	})
 
 }
 
