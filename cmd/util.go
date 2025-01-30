016f8e2b158290d390f319490504565460c78ae9
Ishita Sequeira <46771830+ishitasequeira@users.noreply.github.com>
2025-01-30 15:18:24 -0500

    Initialize the kubeClient and applicationsClientSet correctly (#1025)
    Signed-off-by: Ishita Sequeira <ishiseq29@gmail.com>

--- cmd/util.go
+++ cmd/util.go
@@ -3,12 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
-	"path/filepath"
 	"time"
 
 	"github.com/argoproj-labs/argocd-image-updater/pkg/kube"
-	registryKube "github.com/argoproj-labs/argocd-image-updater/registry-scanner/pkg/kube"
-	"github.com/argoproj-labs/argocd-image-updater/registry-scanner/pkg/log"
 )
 
 func getPrintableInterval(interval time.Duration) string {
@@ -28,29 +25,10 @@ func getPrintableHealthPort(port int) string {
 }
 
 func getKubeConfig(ctx context.Context, namespace string, kubeConfig string) (*kube.ImageUpdaterKubernetesClient, error) {
-	var fullKubeConfigPath string
-	var kubeClient *kube.ImageUpdaterKubernetesClient
-	var err error
 
-	if kubeConfig != "" {
-		fullKubeConfigPath, err = filepath.Abs(kubeConfig)
-		if err != nil {
-			return nil, fmt.Errorf("cannot expand path %s: %v", kubeConfig, err)
-		}
-	}
-
-	if fullKubeConfigPath != "" {
-		log.Debugf("Creating Kubernetes client from %s", fullKubeConfigPath)
-	} else {
-		log.Debugf("Creating in-cluster Kubernetes client")
-	}
-
-	kubernetesClient, err := registryKube.NewKubernetesClientFromConfig(ctx, namespace, fullKubeConfigPath)
+	kubeClient, err := kube.NewKubernetesClient(ctx, kubeConfig, namespace)
 	if err != nil {
 		return nil, err
-	}
-	kubeClient = &kube.ImageUpdaterKubernetesClient{
-		KubeClient: kubernetesClient,
 	}
 
 	return kubeClient, nil
