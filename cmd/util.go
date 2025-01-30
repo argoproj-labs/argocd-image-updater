8a0254b69d2ef5c415beb4f5492b7d431262d35d
Ishita Sequeira <ishiseq29@gmail.com>
2025-01-30 13:23:40 -0500

    address comments
    Signed-off-by: Ishita Sequeira <ishiseq29@gmail.com>

--- cmd/util.go
+++ cmd/util.go
@@ -3,14 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
-	"os"
 	"time"
 
 	"github.com/argoproj-labs/argocd-image-updater/pkg/kube"
-
-	"github.com/argoproj/argo-cd/v2/pkg/client/clientset/versioned"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
 )
 
 func getPrintableInterval(interval time.Duration) string {
@@ -30,37 +25,11 @@ func getPrintableHealthPort(port int) string {
 }
 
 func getKubeConfig(ctx context.Context, namespace string, kubeConfig string) (*kube.ImageUpdaterKubernetesClient, error) {
-	var kubeClient *kube.ImageUpdaterKubernetesClient
 
-	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
-	loadingRules.DefaultClientConfig = &clientcmd.DefaultClientConfig
-	loadingRules.ExplicitPath = kubeConfig
-	overrides := clientcmd.ConfigOverrides{}
-	clientConfig := clientcmd.NewInteractiveDeferredLoadingClientConfig(loadingRules, &overrides, os.Stdin)
-
-	config, err := clientConfig.ClientConfig()
+	kubeClient, err := kube.NewKubernetesClient(ctx, kubeConfig, namespace)
 	if err != nil {
 		return nil, err
 	}
-
-	if namespace == "" {
-		namespace, _, err = clientConfig.Namespace()
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return nil, err
-	}
-
-	applicationsClientset, err := versioned.NewForConfig(config)
-	if err != nil {
-		return nil, err
-	}
-
-	kubeClient = kube.NewKubernetesClient(ctx, clientset, applicationsClientset, namespace)
 
 	return kubeClient, nil
 }
