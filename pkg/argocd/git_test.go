68ab8e2564af099f638bc7de15f852cff2d3c10a
Eidmantas Ivanauskas <eidmantas.ivanauskas@oxylabs.io>
2025-09-17 18:05:32 +0300

    test(git): fix Application annotations construction in grouping test

--- pkg/argocd/git_test.go
+++ pkg/argocd/git_test.go
@@ -3,13 +3,14 @@ package argocd
 import (
     "testing"
     "github.com/argoproj-labs/argocd-image-updater/pkg/common"
+    v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
     v1alpha1 "github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
 )
 
 // Test that grouping does not mix different branches in one commit/push
 func Test_groupIntentsByBranch(t *testing.T) {
-    appA := &v1alpha1.Application{Annotations: map[string]string{common.GitBranchAnnotation: "main:appA-branch"}}
-    appB := &v1alpha1.Application{Annotations: map[string]string{common.GitBranchAnnotation: "main:appB-branch"}}
+    appA := &v1alpha1.Application{ObjectMeta: v1.ObjectMeta{Annotations: map[string]string{common.GitBranchAnnotation: "main:appA-branch"}}}
+    appB := &v1alpha1.Application{ObjectMeta: v1.ObjectMeta{Annotations: map[string]string{common.GitBranchAnnotation: "main:appB-branch"}}}
     wbcA := &WriteBackConfig{GitRepo: "https://example/repo.git", GitWriteBranch: "appA-branch"}
     wbcB := &WriteBackConfig{GitRepo: "https://example/repo.git", GitWriteBranch: "appB-branch"}
 
