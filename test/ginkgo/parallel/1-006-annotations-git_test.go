1c19a38d986ab7ffd1c74c592bda7cb46261ca59
Denis Karpelevich <56302307+dkarpele@users.noreply.github.com>
2026-02-13 01:34:47 +0100

    fix: deprecate spec.namespace (#1490)
    Signed-off-by: dkarpele <karpelevich@gmail.com>

--- test/ginkgo/parallel/1-006-annotations-git_test.go
+++ test/ginkgo/parallel/1-006-annotations-git_test.go
@@ -186,7 +186,6 @@ var _ = Describe("ArgoCD Image Updater Parallel E2E Tests", func() {
 					Namespace: ns.Name,
 				},
 				Spec: imageUpdaterApi.ImageUpdaterSpec{
-					Namespace: ns.Name,
 					ApplicationRefs: []imageUpdaterApi.ApplicationRef{
 						{
 							NamePattern:    "app*",
